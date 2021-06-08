4ccbf70a229ce3dde27f830a4d399041b305fca6
Bienvenue Gwenaelle <gwenaelle.bienvenue@student-cs.fr>
2021-06-08 10:56:26 +0200

    Jour 1 : Front basics

---
+++ frontend/src/components/Movie.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <img
+      alt="movie poster"
+      :src="'https://image.tmdb.org/t/p/w400' + movie.poster_path"
+    />
+    <p>{{ movie.title }}</p>
+    <p>Note moyenne : {{ movie.vote_average }}</p>
+    <p>{{ movie.release_date }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Movie",
+  props: {
+    movie: Array,
+  },
+};
+</script>
+
+<style scoped>
+table {
+  border-collapse: collapse;
+}
+
+th,
+td {
+  border: 1px solid #000000;
+  padding: 10px;
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,77 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <h1>Welcome to Your Vue.js App</h1>
+    <h1>Liste des films</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
       <a href="https://cli.vuejs.org" target="_blank">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank">Forum</a>
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank">Community Chat</a>
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank">vue-router</a>
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank">vue-loader</a>
-      </li>
-      <li>
-        <a href="https://github.com/vuejs/awesome-vue" target="_blank"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+
+    <div class="user-input">
+      <input v-model="movieName" />
+    </div>
+
+    <div class="movie-table">
+      <p v-for="movie in movies" :key="movie.id">
+        <div class="movie-seule">
+          <Movie :movie="movie" />
+        </div>
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
+import Movie from "@/components/Movie.vue";
+import axios from "axios";
+
 export default {
   name: "Home",
+  components: { Movie },
+  data: function () {
+    return {
+      movieName: "",
+      movies: [],
+    };
+  },
+  methods: {
+    fetchMovies: function () {
+      axios
+        .get(
+          "https://api.themoviedb.org/3/movie/popular?api_key=a0a7e40dc8162ed7e37aa2fc97db5654&language=en-US&page=1"
+        )
+        .then((response) => {
+          this.movies = response.data.results;
+          console.log("ok");
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+
+  created() {
+    this.fetchMovies();
+  },
 };
 </script>
 
@@ -79,6 +60,20 @@ export default {
 <style scoped>
 .home {
   text-align: center;
+}
+.movie-seule {
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+.movie-table {
+  text-align: center;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
 }
 
 h3 {
